a427d0f2ee2f3a9567092ac2b673129f02c7bd71
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-05 17:18:20 +0200

    update

--- 8kyu/reversed_words.js
+++ 8kyu/reversed_words.js
@@ -2,12 +2,12 @@
 Complete the solution so that it reverses all of the words within the string passed in.
 */
 const reverseWords = (str) => {
-    const newStr = str.split(' ');
-    const finalStr = [];
-    for (let i = newStr.length - 1; i >= 0; i--) {
-        finalStr.push(newStr[i]);
+    const strArr = str.split(' ');
+    const reversedStr = [];
+    for (let i = strArr.length - 1; i >= 0; i--) {
+        reversedStr.push(strArr[i]);
     }
-    console.log(finalStr.join(' '));
+    console.log(reversedStr.join(' '));
 }
 
 reverseWords('Hello World, my name is balou');
