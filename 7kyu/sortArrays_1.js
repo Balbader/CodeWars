f560131fd959684178f5864e8b52d5e48e6cd1bc
Balbader <15971200+Balbader@users.noreply.github.com>
2021-07-09 12:31:37 +0200

    intitial

--- 7kyu/sortArrays_1.js
+++ 7kyu/sortArrays_1.js
@@ -4,5 +4,14 @@ the input-array / list sorted in lexicographical order.
 */
 
 const sortMe = (arr) => {
-
+	return arr.sort();
 }
+
+console.log(sortMe(['one', 'two', 'three']));
+
+// Variation
+const sortMeNow = (arr) => {
+	return arr.sort((a, b) => a > b ? 1 : a < b ? -1 : 0 );
+}
+
+console.log(sortMeNow(['one', 'two', 'three']));
